206960ea8b427ede7cd40738f42c2e195e059d5d
Bilal Mazhar MS(IS) Cyber Security II Privacy Professional <62200321+BilalMaz@users.noreply.github.com>
2023-11-04 23:50:11 +0500

    s
    s

---
+++ Quater - 1/Typescript/Home - Assignement - 1/Quotes - 2/Quotes.js
@@ -0,0 +1,5 @@
+// Store the famous person's name in a variable
+var famous_person = "Muhammad Ali Jinnah";
+// Store the famous quote in a variable
+var famousQuote = "With faith, discipline, and selfless devotion to duty, there is nothing worthwhile that you cannot achieve.";
+console.log("\"".concat(famousQuote, "\" - ").concat(famous_person));
---
+++ Quater - 1/Typescript/Home - Assignement - 1/Quotes - 2/Quotes.ts
@@ -0,0 +1,7 @@
+// Store the famous person's name in a variable
+const famous_person: string = "Muhammad Ali Jinnah";
+
+// Store the famous quote in a variable
+const famousQuote: string = "With faith, discipline, and selfless devotion to duty, there is nothing worthwhile that you cannot achieve.";
+
+console.log(`"${famousQuote}" - ${famous_person}`);
