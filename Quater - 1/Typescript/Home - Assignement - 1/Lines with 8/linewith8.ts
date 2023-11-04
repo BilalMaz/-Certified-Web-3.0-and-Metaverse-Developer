6467333f45fdcbca0417cfa729d0ca67760a4d1c
Bilal Mazhar MS(IS) Cyber Security II Privacy Professional <62200321+BilalMaz@users.noreply.github.com>
2023-11-05 00:48:58 +0500

    Update linewith8.ts

---
+++ Quater - 1/Typescript/Home - Assignement - 1/Lines with 8/linewith8.ts
@@ -0,0 +1,11 @@
+console.log(5 + 3);
+
+const screenWidth = process.stdout.columns; // Get the width of the terminal
+const line = "-".repeat(screenWidth); // Create a line of dashes
+
+console.log(line);  // Line of dashes
+console.log(10 - 2);
+console.log(line);  // Line of dashes
+console.log(4 * 2);
+console.log(line);  // Line of dashes
+console.log(16 / 2);
