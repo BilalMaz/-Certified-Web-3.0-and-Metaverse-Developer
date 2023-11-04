eb1445ef934ba15034dbb61fdb53582d3f03b8c2
Bilal Mazhar MS(IS) Cyber Security II Privacy Professional <62200321+BilalMaz@users.noreply.github.com>
2023-11-04 23:21:13 +0500

    Create upperlow.ts

---
+++ Quater - 1/Typescript/Home - Assignement - 1/Upper&Lower - Program/upperlow.ts
@@ -0,0 +1,14 @@
+// Store the person's name in a variable using var
+var personName: string = "Bilal Mazhar ";
+
+// Convert the name to lowercase and store it in a variable
+var lowercaseName: string = personName.toLowerCase();
+
+// Convert the name to uppercase and store it in a variable
+var uppercaseName: string = personName.toUpperCase();
+
+
+// Print the names
+console.log("Original Name: " + personName);
+console.log("Lowercase Name: " + lowercaseName);
+console.log("Uppercase Name: " + uppercaseName);
