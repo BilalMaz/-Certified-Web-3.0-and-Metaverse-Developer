cb0c747b69c5855d93fd0b69a72ec48aab51b8d5
Bilal Mazhar MS(IS) Cyber Security II Privacy Professional <62200321+BilalMaz@users.noreply.github.com>
2023-11-04 23:40:04 +0500

    3
    Quotation program

---
+++ Quater - 1/Typescript/Home - Assignement - 1/Quotes/Quotes.js
@@ -0,0 +1,5 @@
+// Store the famous quote and its author
+var famousQuote = "With faith, discipline, and selfless devotion to duty, there is nothing worthwhile that you cannot achieve.";
+var author = "Muhammad Ali Jinnah";
+// Print the quote and its author
+console.log("\"".concat(famousQuote, "\" - ").concat(author));
---
+++ Quater - 1/Typescript/Home - Assignement - 1/Quotes/Quotes.ts
@@ -0,0 +1,6 @@
+// Store the famous quote and its author
+const famousQuote: string = "With faith, discipline, and selfless devotion to duty, there is nothing worthwhile that you cannot achieve.";
+const author: string = "Muhammad Ali Jinnah";
+
+// Print the quote and its author
+console.log(`"${famousQuote}" - ${author}`);
