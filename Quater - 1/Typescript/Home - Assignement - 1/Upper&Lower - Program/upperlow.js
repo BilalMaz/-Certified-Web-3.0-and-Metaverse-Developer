20fd9a28517442c3b45164a9068710eb834fcbdc
Bilal Mazhar MS(IS) Cyber Security II Privacy Professional <62200321+BilalMaz@users.noreply.github.com>
2023-11-04 23:34:40 +0500

    k
    k

--- Quater - 1/Typescript/Home - Assignement - 1/Upper&Lower - Program/upperlow.js
+++ Quater - 1/Typescript/Home - Assignement - 1/Upper&Lower - Program/upperlow.js
@@ -1,13 +1,9 @@
 // Store the person's name in a variable using var
-var personName  = "Bilal Mazhar ";
-
+var personName = "Bilal Mazhar ";
 // Convert the name to lowercase and store it in a variable
 var lowercaseName = personName.toLowerCase();
-
 // Convert the name to uppercase and store it in a variable
 var uppercaseName = personName.toUpperCase();
-
-
 // Print the names
 console.log("Original Name: " + personName);
 console.log("Lowercase Name: " + lowercaseName);
