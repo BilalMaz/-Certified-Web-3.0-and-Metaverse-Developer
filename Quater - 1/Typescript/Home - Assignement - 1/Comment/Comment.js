38950176ad4a466a87e48d78d27a20c10f741cec
Bilal Mazhar MS(IS) Cyber Security II Privacy Professional <62200321+BilalMaz@users.noreply.github.com>
2023-11-05 20:59:37 +0500

    g
    g

---
+++ Quater - 1/Typescript/Home - Assignement - 1/Comment/Comment.js
@@ -0,0 +1,8 @@
+// Program written by [Your Name]
+// Date: [Current Date]
+// this program print helllo world 
+var personName = "Eric";
+// Create a personalized message
+var message = "Hello ".concat(personName, ", would you like to learn some Python today?");
+// Print the message to the console
+console.log(message);
---
+++ Quater - 1/Typescript/Home - Assignement - 1/Comment/Comment.ts
@@ -0,0 +1,11 @@
+// Program written by [Your Name]
+// Date: [Current Date]
+// this program print helllo world 
+
+var personName = "Eric";
+
+// Create a personalized message
+var message = `Hello ${personName}, would you like to learn some Python today?`;
+
+// Print the message to the console
+console.log(message);
